7f3f55eff5df4949fc5fe49cc41a08b7af7a4243
cilliantighe <N00152737@student.iadt.ie>
2019-04-08 22:11:04 +0100

    make it better

--- src/server/index.js
+++ src/server/index.js
@@ -36,8 +36,10 @@ mongoose.connect(
   }
 );
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function(req, res) {
-  res.sendFile(path.join('../../', 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/api/home', function(req, res) {
